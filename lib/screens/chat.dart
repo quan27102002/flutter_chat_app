432741b8e36263b9b8ab87a0c2174e9423923c7b
quanndh <quan.nguyen@vconnex.vn>
2024-08-22 13:45:10 +0700

    final chatapp

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_app/widgets/chat_messages.dart';
 import 'package:flutter_chat_app/widgets/new_messages.dart';
@@ -11,6 +12,19 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  void setupPushNotification() async {
+    final fcm = FirebaseMessaging.instance;
+    await fcm.requestPermission();
+    fcm.getToken();
+    fcm.subscribeToTopic("chat");
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupPushNotification();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
